f3cb170f8d97401c52bd935d67aef6f4b4581776
Albert Kwaśkiewicz <albertkwaskiewicz@gmail.com>
2023-01-25 20:08:24 -0500

    Add prompt_user() and test

--- text-to-morse/test_main.py
+++ text-to-morse/test_main.py
@@ -1,2 +1,10 @@
 import pytest
+import main
+
+
+def test_prompt_user(monkeypatch):
+    monkeypatch.setattr('main.prompt_user', lambda: 'Hi')
+    user_input = main.prompt_user()
+    assert user_input == 'Hi'
+
 
