d0b54497a0cfb3a1cc91b688b3be416f4152ffa1
Albert Kwaśkiewicz <albertkwaskiewicz@gmail.com>
2023-01-30 18:38:20 -0500

    Creating board game with '|' and '-'

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -3,6 +3,20 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+def play_game():
+    is_playing = True
+
+    while is_playing:
+        pass
+
+def create_game_board():
+    return '''
+       |   |   
+    -----------
+       |   |   
+    -----------
+       |   |   
+    '''
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
