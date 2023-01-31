ca47b7bf7a6506812499716d276fcc0ed0e726d7
Albert Kwaśkiewicz <albertkwaskiewicz@gmail.com>
2023-01-31 16:34:08 -0500

    Update create_game_board() to eliminate multi-line string Create functionality in update_board() Add is_game_over()

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -2,24 +2,40 @@ def play_game():
     is_playing = True
     board = create_game_board()
     is_player_1_turn = True
+    signs = create_sign_container()
 
     while is_playing:
         print(f'Current player: {"Player 1 - X" if is_player_1_turn else "Player 2 - O"}')
         print(board)
         sign = "X" if is_player_1_turn else "O"
-        location = get_sign_location_from_user()
+        # location = get_sign_location_from_user()
 
         is_player_1_turn = not is_player_1_turn
 
 
 def create_game_board():
-    return '''
-       |   |   
-    -----------
-       |   |   
-    -----------
-       |   |   
-    '''
+    new_board = ''
+    split = '---------'
+    for num in range(1, 10):
+        if num % 3 == 0:
+            new_board += f'  |  |  \n'
+            if num != 9:
+                new_board += f'{split}\n'
+    return new_board
+
+
+def create_sign_container():
+    return [''] * 9
+
+
+def assign_sign(sign, container: list, board):
+    location = get_sign_location_from_user()
+    if container[location - 1] == '':
+        container[location - 1] = sign
+        container.insert()
+    else:
+        print("That spot is already taken: Try again.")
+        assign_sign(sign, container, board)
 
 
 def get_sign_location_from_user():
@@ -32,8 +48,20 @@ def get_sign_location_from_user():
         get_sign_location_from_user()
 
 
-def create_piece_container():
-    return [''] * 9
+def update_board(container):
+    new_board = ''
+    split = '---------'
+
+    for index, _ in enumerate(container):
+        if (index + 1) % 3 == 0:
+            new_board += f' {container[index - 2]} | {container[index - 1]} | {container[index]} \n'
+            if (index + 1) != len(container):
+                new_board += f'{split}\n'
+    return new_board
+
+
+def is_game_over():
+    pass
 
 
 def reset_game():
