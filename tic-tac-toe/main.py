d76ebe9b628be7d48192ca954d585e907a4b5be4
Albert Kwaśkiewicz <albertkwaskiewicz@gmail.com>
2023-01-30 20:12:53 -0500

    Add functionality for getting location of player sign

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,13 +1,15 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 def play_game():
     is_playing = True
+    board = create_game_board()
+    is_player_1_turn = True
 
     while is_playing:
-        pass
+        print(f'Current player: {"Player 1 - X" if is_player_1_turn else "Player 2 - O"}')
+        print(board)
+        sign = "X" if is_player_1_turn else "O"
+        location = get_piece_location_from_user()
+
+        is_player_1_turn = not is_player_1_turn
 
 
 def create_game_board():
@@ -18,6 +20,18 @@ def create_game_board():
     -----------
        |   |   
     '''
+
+
+def get_piece_location_from_user():
+    return int(input("Provide where to write your sign. Only numbers 1 - 9 accepted. (Going from left to right).\n"))
+
+
+def create_piece_container():
+    return [''] * 9
+
+
+def reset_game():
+    pass
 
 
 if __name__ == '__main__':
