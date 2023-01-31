c39ba85cd6ab4a382fe925d8faf6787e593d7b7c
Albert Kwaśkiewicz <albertkwaskiewicz@gmail.com>
2023-01-31 14:52:43 -0500

    Add tests for assigning sign, updating board, and checking if game is over

--- tic-tac-toe/test_main.py
+++ tic-tac-toe/test_main.py
@@ -2,9 +2,13 @@ import pytest
 from main import *
 
 
-def test_create_game_board():
-    board = create_game_board()
-    assert board == '''
+@pytest.fixture()
+def empty_board():
+    return create_game_board()
+
+
+def test_create_game_board(empty_board):
+    assert empty_board == '''
        |   |   
     -----------
        |   |   
@@ -13,12 +17,34 @@ def test_create_game_board():
     '''
 
 
-def test_create_piece_container():
-    container = create_piece_container()
+def test_create_sign_container():
+    container = create_sign_container()
     assert container == ['', '', '', '', '', '', '', '', '']
 
 
-def test_get_piece_location_from_user(monkeypatch):
+def test_get_sign_location_from_user(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: '3')
-    location = get_piece_location_from_user()
+    location = get_sign_location_from_user()
     assert location == 3
+
+
+def test_assign_sign(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: '3')
+    board = create_game_board()
+    container = create_sign_container()
+    assign_sign('X', container, board)
+    assert board == '''
+       |   | X 
+    -----------
+       |   |   
+    -----------
+       |   |   
+    '''
+
+
+def test_update_board():
+    pass
+
+
+def test_is_game_over():
+    pass
